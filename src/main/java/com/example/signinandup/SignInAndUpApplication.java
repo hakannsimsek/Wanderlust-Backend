f1562d33f7c2e6b05c7cb596ce68455a7ccb8602
hhakann <74412503+hhakann@users.noreply.github.com>
2023-05-20 22:42:55 +0300

    init

---
+++ src/main/java/com/example/signinandup/SignInAndUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.signinandup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignInAndUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignInAndUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signinandup/SignInAndUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signinandup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignInAndUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
